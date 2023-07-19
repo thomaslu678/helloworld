5a76d2a8404fe4983d6596a810772bfe3a3ad2ca
thomaslu678 <thomaslu678@gmail.com>
2023-07-18 18:50:55 -0600

    changes for vercel

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v=zajt@9h%ds2d^)p*1+&p)e=dwgs_a!aa*%mv@aq%du5#(hy='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'helloworld.wsgi.application'
+WSGI_APPLICATION = 'helloworld.wsgi.app'
 
 
 # Database
